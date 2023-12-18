656a23a4f665cc8140e79dacb2470bfa75319b3d
Script Raccoon <scriptraccoon@gmail.com>
2023-12-18 23:28:02 +0100

    meta tags

--- client/src/routes/+layout.svelte
+++ client/src/routes/+layout.svelte
@@ -5,6 +5,13 @@
 
 <svelte:head>
 	<title>{TITLE}</title>
+	<meta property="og:title" content="Chessvelte" />
+	<meta property="og:description" content="Play chess with anyone" />
+	<meta name="description" content="Play chess with anyone" />
+	<meta property="og:type" content="Website" />
+	<meta property="og:url" content="https://chessvelte.netlify.app" />
+	<meta property="og:site_name" content="Chessvelte" />
+	<meta property="og:image" content="./board.png" />
 </svelte:head>
 
 <slot />
