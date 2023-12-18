6ba17ad792d6a1ff06174f5c265205d6dede4085
Script Raccoon <scriptraccoon@gmail.com>
2023-12-19 00:17:40 +0100

    make jest understand path aliases

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,9 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-};
+	preset: "ts-jest",
+	testEnvironment: "node",
+	moduleDirectories: ["node_modules", "<rootDir>"],
+	moduleNameMapper: {
+		"^\\$shared/(.*)$": "<rootDir>/shared/$1",
+	},
+}
---
+++ server/src/controllers/Player.spec.ts
@@ -0,0 +1,28 @@
+import { Player } from "./Player"
+
+describe("Player class", () => {
+	it("constructs a player with prescribed properties", () => {
+		const player = new Player("123", "white", "mary")
+		expect(player.client_id).toBe("123")
+		expect(player.color).toBe("white")
+		expect(player.name).toBe("mary")
+	})
+})
+
+describe("switch_color", () => {
+	it("changes the color of a player from white to black and vice versa", () => {
+		const player = new Player("123", "white", "mary")
+		player.switch_color()
+		expect(player.color).toBe("black")
+		player.switch_color()
+		expect(player.color).toBe("white")
+	})
+})
+
+describe("set_name", () => {
+	it("sets the name accordingly to the input", () => {
+		const player = new Player("123", "white", "mary")
+		player.set_name("maria")
+		expect(player.name).toBe("maria")
+	})
+})
