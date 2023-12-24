76015126703d24ab937f9c4c6fd59894c8e4fc88
Script Raccoon <scriptraccoon@gmail.com>
2023-12-24 04:18:05 +0100

    custom error page

---
+++ client/src/routes/+error.svelte
@@ -0,0 +1,26 @@
+<script>
+	import { page } from "$app/stores"
+	import Title from "$lib/components/Title.svelte"
+</script>
+
+<Title />
+
+<main>
+	<h2>Error: {$page.error?.message}</h2>
+
+	<p>
+		<a href="/">Home</a>
+	</p>
+</main>
+
+<style>
+	main {
+		text-align: center;
+	}
+	h2 {
+		color: var(--error-color);
+		font-weight: 300;
+		font-size: 1.25rem;
+		padding-block: 2rem 1rem;
+	}
+</style>
